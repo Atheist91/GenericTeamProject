88bdb2605fd1cbe4df24aa6c338aa8ae37df24d7
Vuko <drakkainen1991@gmail.com>
2016-05-16 02:08:13 +0200

    NEW: Visibility of ComboBox depends on whether there is a pin connected to FTeam's pin or not. NEW: Selection is persistent even after reopening editor. NEAT! :D

--- Source/GenericTeamProjectEditor/STeamPin.cpp
+++ Source/GenericTeamProjectEditor/STeamPin.cpp
@@ -1,74 +1,106 @@
 #include "GenericTeamProjectEditor.h" 
 #include "KismetEditorUtilities.h" 
-#include "STextComboBox.h"
 #include "EdGraph/EdGraphPin.h"
 #include "EdGraph/EdGraphSchema.h"
 #include "SGraphPin.h"
+#include "SNameComboBox.h"
+#include "STextComboBox.h"
 
 #include "STeamPin.h"
 #include "CustomTypes.h"
 
-void STeamPin::Construct(const FArguments& InArgs, UEdGraphPin* InGraphPinObj)
-{ 
-	// Getting DataTable containing info about Teams.
-	UDataTable* DT = UCustomTypes::GetTeamsDataTable();
-	if (DT)
-	{
-		// Building list of options that will be available in combo box widget.
-		for (FName TeamName : DT->GetRowNames())
-		{
-			FString* Str = new FString(TeamName.ToString());
-			TeamsList.Add(MakeShareable(Str));
-		}
-	}
-
-	SGraphPin::Construct(SGraphPin::FArguments(), InGraphPinObj);
-}
-
-TSharedRef<SWidget>	STeamPin::GetDefaultValueWidget()
+void STeamPin::Construct(const FArguments & InArgs, UEdGraphPin * InGraphPinObj)
 {
-	return SNew(STextComboBox)
-
-		// Setting source of options available in combo box widget
-		.OptionsSource(&TeamsList)
-
-		// Singing up for event fired whenever user selects an option from combo box.
-		.OnSelectionChanged(this, &STeamPin::OnTeamSelected);
+	SGraphPin::Construct(SGraphPin::FArguments(), InGraphPinObj);
 }
 
 void STeamPin::OnTeamSelected(TSharedPtr<FString> InSelectedString, ESelectInfo::Type InSelectionType)
 {
-	FString CurrentDefaultValue = GraphPinObj->GetDefaultAsString();
-
-	FString SelectedTeamName = *InSelectedString;
-
-	if (CurrentDefaultValue.IsEmpty())
+	FString Name = InSelectedString.IsValid() ? *InSelectedString : FString("");
+	if (auto Schema = (GraphPinObj ? GraphPinObj->GetSchema() : nullptr))
 	{
-		CurrentDefaultValue = FString(TEXT("Purple"));
+		// If we selected different Team from the ComboBox
+		if (GraphPinObj->GetDefaultAsString() != Name)
+		{
+			// Getting prepared string for 
+			FString TeamNameString = GetPreparedString(Name);
+
+			// Editing variable under pin
+			GraphPinObj->Modify();
+			Schema->TrySetDefaultValue(*GraphPinObj, TeamNameString);
+		}
+	}
+}
+
+TSharedRef<SWidget>	STeamPin::GetDefaultValueWidget()
+{
+	// Will contain currently selected Name
+	TSharedPtr<FString> CurrentlySelectedName;
+
+	// Building list of available names in drop down
+	{
+		// Getting DataTable containing info about Teams
+		UDataTable* DT = UCustomTypes::GetTeamsDataTable();
+		if (DT)
+		{
+			// Getting previously selected string.
+			// It'll be formatted as PreparedString (see GetPreparedString function).
+			FString PreviousSelection = GraphPinObj->GetDefaultAsString();
+
+			// Building list of options that will be available in combo box widget
+			for (FName TeamName : DT->GetRowNames())
+			{
+				TSharedPtr<FString> TempTeamString = MakeShareable(new FString(TeamName.ToString()));
+
+				// Adding Team to the list
+				TeamsList.Add(TempTeamString);
+
+				// Prepared string for current Team
+				FString TempPreparedString = GetPreparedString(*TempTeamString.Get());
+
+				// If previously selected Team still exist in available options in ComboBox
+				if (PreviousSelection == TempPreparedString)
+				{
+					CurrentlySelectedName = TempTeamString;
+				}
+			}
+		}
 	}
 
-	// Setting up 
-	FString TeamNameString = TEXT("(");
-	if (!SelectedTeamName.IsEmpty())
+	return SNew(STextComboBox)
+
+		// Aesthetic stuff
+		.ContentPadding(FMargin(6.0f, 2.0f))
+
+		// Setting source of available options in combo box. The list is generated above.
+		.OptionsSource(&TeamsList)
+
+		// Singing up for event fired whenever user selects an option from combo box.
+		.OnSelectionChanged(this, &STeamPin::OnTeamSelected)
+
+		// Setting up what Name is shown as selected initially. 'CurrentlySelectedName' is determined above.
+		.InitiallySelectedItem(CurrentlySelectedName)
+		
+		// Binding visibility of the ComboBox to the given function.
+		.Visibility(this, &STeamPin::GetComboBoxVisibility)
+
+		;
+}
+
+EVisibility STeamPin::GetComboBoxVisibility() const
+{
+	// We want to show combo box only, if pin is an input one AND if we didn't plug in any other pins into this one.
+	if (GraphPinObj->Direction == EEdGraphPinDirection::EGPD_Input && GraphPinObj->LinkedTo.Num() == 0)
 	{
-		//now set here proerty name from USTRUCT(), \" - will add opening "
-		// so it will look like AttributeName="
-
-		TeamNameString += TEXT("Name=\"");
-		//add value you want to set to your property"
-		TeamNameString += SelectedTeamName;
-		//close with "
-		TeamNameString += TEXT("\"");
+		return EVisibility::Visible;
 	}
-
-	//and at last add ) so it will look like (AttributeName="Value");
-	TeamNameString += TEXT(")");
-
-    //and here we set our value to parameter if it different than last one.
-	if (!CurrentDefaultValue.Equals(TeamNameString))
+	else
 	{
-		auto* schema = GraphPinObj->GetSchema();
-
-		GraphPinObj->GetSchema()->TrySetDefaultValue(*GraphPinObj, TeamNameString);
+		return EVisibility::Collapsed;
 	}
+}
+
+FString STeamPin::GetPreparedString(FString InTeamName) const
+{
+	return FString::Printf(TEXT("(Name=\"%s\")"), *InTeamName);
 }
--- Source/GenericTeamProjectEditor/STeamPin.h
+++ Source/GenericTeamProjectEditor/STeamPin.h
@@ -13,11 +13,19 @@ protected:
 
 	TArray<TSharedPtr<FString>> TeamsList;
  
-public:
+public:	
 
 	void Construct(const FArguments& InArgs, UEdGraphPin* InGraphPinObj);
 
+	virtual void OnTeamSelected(TSharedPtr<FString> InSelectedString, ESelectInfo::Type InSelectionType);
+
 	virtual TSharedRef<SWidget>	GetDefaultValueWidget() override;
 
-	void OnTeamSelected(TSharedPtr<FString> InSelectedString, ESelectInfo::Type InSelectionType);
+	// Determines in what scenario ComboBox is supposed to be visible or not.
+	virtual EVisibility GetComboBoxVisibility() const;
+
+protected:
+
+	// Creates string that is ready to put in pin, so it'll override the content of FTeam.Name property.
+	FString GetPreparedString(FString InTeamName) const;
 };
